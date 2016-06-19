a0950afdae8e4594e10cb83d0bb6e26ca7374f51
ShawnFiend <johnxu2010cd@gmail.com>
2016-06-20 00:05:37 +0800

    sencond day @ 25-bootstrap and static files

--- music/urls.py
+++ music/urls.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
 # @Author: ShawnFiend
 # @Date:   2016-06-18 17:58:23
-# @Last Modified by:   ShawnFiend
-# @Last Modified time: 2016-06-19 01:12:03
+# @Last Modified by:   root
+# @Last Modified time: 2016-06-19 17:18:38
 
 from django.conf.urls import url
 from . import views
+
+app_name = 'music'
 
 urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
 
-    # /music/31
+    # /music/<album_id>/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 
-    # /m
+    # /music/<album_id>/favorite
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+
 ]
 
