57a4c9fcf682dcfca052c7fb2dccfd3105cd7fd3
ShawnFiend <johnxu2010cd@gmail.com>
2016-06-20 17:21:37 +0800

    31-modelform and createview

--- music/urls.py
+++ music/urls.py
@@ -2,7 +2,7 @@
 # @Author: ShawnFiend
 # @Date:   2016-06-18 17:58:23
 # @Last Modified by:   root
-# @Last Modified time: 2016-06-19 17:18:38
+# @Last Modified time: 2016-06-20 16:31:57
 
 from django.conf.urls import url
 from . import views
@@ -11,13 +11,16 @@ app_name = 'music'
 
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
     # /music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
+    # /music/
+    url(r'^album/add/$', views.AlbumCreate.as_view(), name='album-add')
 
     # /music/<album_id>/favorite
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    # url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 
 ]
 
